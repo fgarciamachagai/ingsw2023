2998b817a3b78fde4b3ac96d23377772ca1ebce0
fgarciamachagai <fgarcia.machagai@gmail.com>
2023-06-11 00:38:53 -0300

    primer commit, push y merge

---
+++ ingsw2023.test/SumadorShould.cs
@@ -0,0 +1,30 @@
+﻿using ingsw2023.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ingsw2023.test
+{
+    public class SumadorShould
+    {
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(1, 1, 2)]
+        [InlineData(4, 6, 10)]
+
+        public void SumarNumerosShould(int primerSumando, int segundoSumando, int resulEsperado)
+        {
+            //Arrange
+            Sumador sumador = new Sumador();
+
+            //Act
+            int resulSuma = sumador.SumarNumeros(primerSumando, segundoSumando);
+
+            //Assert
+            Assert.Equal(resulEsperado, resulSuma);
+        }
+    }
+}
